8826bee7637cb3aca7e8e727e0050b3c5d8e315d
DataExplorerUser <54912887+DataExplorerUser@users.noreply.github.com>
2021-10-28 13:59:38 +0200

    Add files via upload

---
+++ flip images.py
@@ -0,0 +1,22 @@
+from os import listdir
+from PIL import Image
+
+def get_music_files(folder):
+    file_names = []
+    for file in listdir(folder):
+        if file.endswith('.png'):
+            file_names.append(file)
+    return file_names
+
+def flip(image):
+    img = Image.open(image)
+    flip_img = img.transpose(Image.FLIP_LEFT_RIGHT) # flip anti-clockwise
+    # flip_img.show()
+    new_image_name = 'left_' + image
+    flip_img.save(new_image_name)
+
+images = get_music_files('C:/Users/Wiebe/PycharmProjects/DPG_update/Raccoon Music Player TO DO/raccoon COPY')
+
+# flip(images[0])
+for image in images:
+     flip(image)
